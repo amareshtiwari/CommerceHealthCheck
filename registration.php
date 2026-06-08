d802d19242227ed87208e9f822665f5548bf720f
Amaresh <amaresh.tiwari@greenhonchos.com>
2026-06-08 17:31:48 +0530

    Fix registration.php for app/code bootstrap compatibility

--- registration.php
+++ registration.php
@@ -2,10 +2,8 @@
 
 declare(strict_types=1);
 
-use Magento\Framework\ComponentRegistrar;
-
-ComponentRegistrar::register(
-    ComponentRegistrar::MODULE,
+\Magento\Framework\ComponentRegistrar::register(
+    \Magento\Framework\ComponentRegistrar::MODULE,
     'Amaresh_CommerceHealthCheck',
     __DIR__
 );
